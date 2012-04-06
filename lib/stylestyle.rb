6123ac417f10783531ce99cc3f7ff9701ea141eb
Conan Rimmer <conanrimmer@gmail.com>
2012-04-06 14:56:56 -0400

    add loadpath and yaml req

--- lib/stylestyle.rb
+++ lib/stylestyle.rb
@@ -1,3 +1,8 @@
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__))) unless
+  $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
+  
+require 'yaml'
+
 module StyleStyle
   class Runner
     def self.run_cane
