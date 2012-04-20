c7b521f6f770f68417418a764f6cf0dd456effe6
Charles Strahan <charles.c.strahan@gmail.com>
2012-04-20 16:12:54 -0400

    Minor code teaks.

--- lib/stylestyle/cli.rb
+++ lib/stylestyle/cli.rb
@@ -1,16 +1,18 @@
-module StyleStyle::Cli
-  class CliResult < Struct.new(:stdout, :stderr, :exit_status)
-    def exit_code
-      exit_status.exitstatus
+module StyleStyle
+  module Cli extend self
+    class CliResult < Struct.new(:stdout, :stderr, :exit_status)
+      def exit_code
+        exit_status.exitstatus
+      end
     end
-  end
 
-  def self.sh(cmd)
-    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-      pid = wait_thr.pid # pid of the started process.
-      exit_status = wait_thr.value
+    def sh(cmd)
+      Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+        pid = wait_thr.pid # pid of the started process.
+        exit_status = wait_thr.value
 
-      CliResult.new(stdout.read, stderr.read, exit_status)
+        CliResult.new(stdout.read, stderr.read, exit_status)
+      end
     end
   end
 end
