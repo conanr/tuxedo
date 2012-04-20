2564e36fc7fd1569afba81f41a28b78034d887c2
Charles Strahan <charles.c.strahan@gmail.com>
2012-04-20 16:28:14 -0400

    Tweak the rake task.

--- lib/stylestyle/rake_task.rb
+++ lib/stylestyle/rake_task.rb
@@ -7,9 +7,13 @@ module StyleStyle
     include ::Rake::DSL if defined?(::Rake::DSL)
 
     attr_accessor :name
+    attr_accessor :fail_on_error
 
     def initialize(*args)
       @name = args.shift || :style
+      @fail_on_error = true
+
+      yield self if block_given?
 
       task name do
         StyleStyle.output_to_console
